af6d063e747151231867b766d0ec732fc2e9eec7
am23 <am@cloudify.biz>
2021-05-30 20:35:43 +0530

    🔥  Favicon

--- src/components/Navbars/IndexNavbar.vue
+++ src/components/Navbars/IndexNavbar.vue
@@ -32,12 +32,13 @@
         <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
 
           <li class="flex items-center">
-            <button
+            <a
+            href="https://einfach.tech/"
               class="bg-emerald-500 text-white active:bg-emerald-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
               type="button"
             >
               <i class="fas fa-tv mr-2"></i> Admin Panel
-            </button>
+            </a>
           </li>
         </ul>
       </div>
